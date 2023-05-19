835232da86d955071538d39d71018d4f0b983c79
K-K-L-L <124736630+K-K-L-L@users.noreply.github.com>
2023-05-19 18:12:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,30 +12,3 @@ setuptools.setup(
     package_dir={"": "src"},
     install_requires=["httpx"]
 )
-
-# from setuptools import setup, find_packages
-
-# setup(
-#     name='enhanced_google_search',
-#     version='0.1',
-#     description='An enhanced google search library',
-#     long_description=open('README.md').read(),
-#     long_description_content_type='text/markdown',
-#     author='Rian',
-#     author_email='rianmakt2008@gmail.com',
-#     url='https://github.com/K-K-L-L/google_search_py',
-#     packages=find_packages('src'),
-#     package_dir={'': 'src'},
-#     install_requires=[
-#         'httpx'
-#     ],
-#     classifiers=[
-#         'Development Status :: 3 - Alpha',
-#         'Intended Audience :: Developers',
-#         'License :: OSI Approved :: MIT License',
-#         'Programming Language :: Python :: 3',
-#         'Programming Language :: Python :: 3.7',
-#         'Programming Language :: Python :: 3.8',
-#         'Programming Language :: Python :: 3.9',
-#     ],
-# )
